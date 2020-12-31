936d3e0ecc5412dfadc562335a6f9aa960817b4b
Roger Walker <roger.john.walker@gmail.com>
2020-12-31 16:23:39 -0500

    updated title

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Fibonacci Calculator</h1>
+          <h1 className="App-title">Fibonacci Calculator : Hosted in GKE K8s</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
